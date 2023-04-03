2127df867736c69abf6e992b31144089b8ff7641
Heechan <sorrymommy@gmail.com>
2023-04-03 11:51:05 +0900

    완료코드 제거

--- src/main/java/com/sorrymommy/htmlcontent/HtmlContentLoader.java
+++ src/main/java/com/sorrymommy/htmlcontent/HtmlContentLoader.java
@@ -12,14 +12,12 @@ public class HtmlContentLoader {
     public static final String METHOD_GET = "GET";
     public static final String CONTENT_TYPE = "Content-type";
     public static final String APPLICATION_JSON = "application/json";
-    public static final String RESPONSE_CODE_S = "Response code: %s";
     public static final String HTTP_ERROR_CODE_S = "HTTP error code : %s";
 
     public String getContent(URL url) throws IOException {
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod(METHOD_GET);
         conn.setRequestProperty(CONTENT_TYPE, APPLICATION_JSON);
-        System.out.println(String.format(RESPONSE_CODE_S, conn.getResponseCode()));
 
         if (conn.getResponseCode() != HttpsURLConnection.HTTP_OK)
             throw new IOException(String.format(HTTP_ERROR_CODE_S, conn.getResponseCode()));
